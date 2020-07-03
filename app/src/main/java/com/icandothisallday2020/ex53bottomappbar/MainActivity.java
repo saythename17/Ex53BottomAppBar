517ef160cbfb7b8f372443f3c99af52d26c14ff8
FANXY_ON <zion.h719@gmail.com>
2020-07-03 15:43:42 +0900

    Initial commit

---
+++ app/src/main/java/com/icandothisallday2020/ex53bottomappbar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.icandothisallday2020.ex53bottomappbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+
+public class MainActivity extends AppCompatActivity {
+    BottomAppBar bab;
+    boolean isCenter=true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bab=findViewById(R.id.bab);
+        setSupportActionBar(bab);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//[←     ]
+    }
+
+    public void clickFab(View view) {
+        isCenter=!isCenter;
+        if(isCenter) bab.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
+        else bab.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_END);
+    }
+}
